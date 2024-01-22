6900371a8cd57fcb69762c65dd16526039da643c
JAHC-DeV <109252740+JAHC-DeV@users.noreply.github.com>
2024-01-22 11:44:57 -0500

    Agregar archivos de proyecto.

---
+++ Simple.PatchLib/Converters/DateTimeConverter.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Simple.PatchLib.Converters
+{
+    public class DateTimeConverter : JsonConverter<DateTime>
+    {
+        private readonly string _format;
+        public DateTimeConverter(string format)
+        {
+            _format = format;
+        }
+        public override DateTime Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        {
+            if (reader.TokenType == JsonTokenType.String)
+            {
+                if (DateTime.TryParseExact(reader.GetString(), _format, null, DateTimeStyles.None, out var result))
+                {
+                    return result;
+                }
+            }
+
+            // Si no puede ser parseado, se lanza una excepción
+            throw new JsonException("Invalid date format");
+        }
+
+        public override void Write(Utf8JsonWriter writer, DateTime value, JsonSerializerOptions options)
+        {
+            writer.WriteStringValue(value.ToString(_format));
+        }
+    }
+}
---
+++ Simple.PatchLib/Patch.cs
@@ -0,0 +1,50 @@
+﻿using Simple.PatchLib.Converters;
+using System.Text.Json;
+
+namespace Simple.PatchLib;
+public sealed class Patch<TModel> : Dictionary<string, object> where TModel : class
+{
+    public TModel Apply(TModel model)
+    {
+        var typeModel = typeof(TModel);
+
+        foreach (var item in this)
+        {
+            var property = typeModel.GetProperty(item.Key);
+            if (property is not null)
+            {
+                var propertyType = property.PropertyType;
+                Console.WriteLine(property.PropertyType.Name);
+                var jsonEle = (JsonElement)item.Value;
+                var deserializedValue = JsonSerializer.Deserialize(jsonEle.GetRawText(), propertyType, new JsonSerializerOptions
+                {
+                    Converters = { new DateTimeConverter("d/M/yyyy HH:mm:ss") },
+                    PropertyNameCaseInsensitive = true,
+                });
+                object convertedValue;
+
+                // Manejar el caso de tipos nullable
+                if (propertyType.IsGenericType && propertyType.GetGenericTypeDefinition() == typeof(Nullable<>))
+                {
+                    // Obtener el tipo base de Nullable
+                    var underlyingType = Nullable.GetUnderlyingType(propertyType);
+                    // Convertir el valor a Nullable
+                    convertedValue = Convert.ChangeType(deserializedValue, underlyingType);
+                }
+                else
+                    convertedValue = Convert.ChangeType(deserializedValue, propertyType);
+                // Manejar la conversión de tipos correctamente
+
+
+                // Asignar el valor convertido a la propiedad
+                property.SetValue(model, convertedValue);
+            }
+        }
+
+        return model;
+    }
+}
+
+
+
+
